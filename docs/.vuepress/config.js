846824b3c8e99432974fd425f0c80289d0814f58
zhouxiao <764800230@qq.com>
2019-11-10 17:47:13 +0800

    test

--- docs/.vuepress/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    title: 'Seven',
-    description: 'Seven的博客',
-    base: '/docs/.vuepress/dist',
-    head: [
-        ['link', { rel: 'icon', href: '/img/logo.ico' }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ]
-}
